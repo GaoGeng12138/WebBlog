4d5632256640f5470cea21c5c0e6388126f2ad0a
zhaoshijie <gaog.zhao@yechtech.com>
2026-03-19 15:25:24 +0800

    fix: 修复后端 VO 类缺失 @NotBlank 导入导致的编译错误
    - 修复 weblog-admin 模块中 PublishArticleReqVO 和 UpdateArticleReqVO 的导入问题
    - 修复 weblog-web 模块中 FrontendUpdateArticleReqVO 的导入问题
    - 经验证，全量编译 mvn clean compile 已成功通过

--- webLog-springboot/weblog-admin/src/main/java/com/gaog/weblog/admin/model/vo/article/PublishArticleReqVO.java
+++ webLog-springboot/weblog-admin/src/main/java/com/gaog/weblog/admin/model/vo/article/PublishArticleReqVO.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.List;
